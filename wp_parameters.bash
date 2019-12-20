cc7ecf8dc84dee3e2af42646e108616fda76843e
ezahr <bosch.peter@icloud.com>
2019-12-20 22:43:24 +0100

    params

--- wp_parameters.bash
+++ wp_parameters.bash
@@ -87,23 +87,70 @@ fi
 
 # //////////////////////////////////////////////////////////////////////////////////////////
 #echo "#######################"
-#echo "##  setters 
+#echo "##  AZURE  https://portal.azure.com/#home
+# alles gebeurt via het web; aan de server-kant. 
+# Ook toegang tot bestanden en mappen gebeurt via een web-interface, 
+# er zijn APIs beschikbaar of er is toegang mogelijk middels andere protocollen,
+# zoals: FTP, SFTP en SSH. Het Azure Services Platform gebruikt een aangepast 
+# besturingssysteem Microsoft Azure om een cluster van servers te beheren die in het datacenter van Microsoft staan. 
 #echo "#######################" 
 
-CERT_HOST_IP=discipl.westeurope.azurecontainer.io  #FQDN linux
-CERT_HOST_IP_WAARDEPAPIEREN_SERVICE_HOSTNAME=discipl.westeurope.azurecontainer.io
-AZ_DNSNAMELABEL=discipl  # `$CERT_HOST_IP | cut -d'.' -f 1`   #waardepapieren or disciple
+
+AZ_DNSNAMELABEL=discipl
+
+#TARGET_HOST=linux_VM
+
+TARGET_HOST=azure_container_instance
+
+ if [ "$TARGET_HOST" = "linux_VM" ]; then
+       echo expression evaluated as linux_VM
+       AZ_TLD=cloudapp.azure.com
+ fi
+
+if [ "$TARGET_HOST" = "azure_container_instance" ]; then
+       echo expression evaluated as azure_container_instance
+       AZ_TLD=azurecontainer.io
+ fi
+
+CERT_HOST_IP=$AZ_DNSNAMELABEL.westeurope."$AZ_TLD"  #FQDN linux
+CERT_HOST_IP_WAARDEPAPIEREN_SERVICE_HOSTNAME=$AZ_DNSNAMELABEL.westeurope.$AZ_TLD
+  # `$CERT_HOST_IP | cut -d'.' -f 1`   #waardepapieren or disciple
 
 #EPHEMERAL_RETENTION_TIME=86400  #24h 
 EPHEMERAL_RETENTION_TIME=2592001 #30 dage
 
-CMD_CONTAINER_STOP_AND_PRUNE=false
+
+#echo "#######################"
+#echo "## DOWNLOAD  
+#echo "#######################" 
+
+GITHUB_DIR=$HOME_DIR/Dropbox/Github/Waardepapieren-AZURE-ACI  #git clone https://github.com/ezahr/Waardepapieren-AZURE-ACI.git 
+PROJECT_DIR=$HOME_DIR/Projects/scratch/virtual-insanity       #git clone https://github.com/disciplo/waardepapieren.git
+DOCKER_COMPOSE_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren
+CLERK_FRONTEND_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/clerk-frontend
+#CLERK_FRONTEND_NGINX_DIR=$CLERK_FRONTEND_DIR/nginx
+#CLERK_FRONTEND_CYPRESS_DIR=$CLERK_FRONTEND_DIR/cypress
+WAARDEPAPIEREN_SERVICE_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/waardepapieren-service
+WAARDEPAPIEREN_SERVICE_CONFIG_DIR=$WAARDEPAPIEREN_SERVICE_DIR/configuration
+
+
+#echo "#######################"
+#echo "## CLONE
+#echo "#######################" 
+
+CMD_CONTAINER_STOP=false
 CMD_IMAGE_REMOVE=false
+CMD_CONTAINER_PRUNE=false
 CMD_GIT_CLONE=false
 
 #echo "#######################"
-#echo "## Dockerfile  configuration "
+#echo "## BUILD
 #echo "#######################" 
+
+CMD_DOCKER_COMPOSE=true  #volumes and links depreciated
+CMD_DOCKER_BUILD=false  # build by container
+CMD_DOCKER_COMPOSE_BUILD=" --build"
+
 
 SET_DOCKERCOMPOSE_TRAVIS_WITHOUT_VOLUME=true       
 SET_DOCKERFILE_CLERK_FRONTEND_WITHOUT_VOLUME=true
@@ -117,39 +164,24 @@ SET_WAARDEPAPIEREN_SERVICE_CONFIG_COMPOSE_TRAVIS_JSON=true
 SET_CLERK_FRONTEND_NGINX_CONF=true
 
 #echo "#######################"
-#echo "## Dockerfile  build and ship 
+#echo "## SHIP 
 #echo "#######################" 
-
-CMD_DOCKER_COMPOSE=true  #volumes and links depreciated
-CMD_DOCKER_BUILD=false  # build by container
-CMD_DOCKER_COMPOSE_BUILD=" --build"
 
 DOCKER_TAG=true
 DOCKER_USER="boscp08"  #NB repository name must be lowercase
-DOCKER_VERSION_TAG="3.0"
+DOCKER_VERSION_TAG="2.0"
 DOCKER_PUSH=true  #hub.docker.com 
 
+
 #echo "#######################"
-#echo "## AZURE CONTAINER INSTANCE   setters"
+#echo "## DEPLOY
 #echo "#######################" 
 
 AZ_RESOURCE_GROUP="Discipl_Wigo4it_DockerGroup2"
-#AZ_DNSNAMELABEL=  # `$CERT_HOST_IP | cut -d'.' -f 1`   #waardepapieren or disciple
-
 AZ_RESOURCE_GROUP_DELETE=true
 AZ_RESOURCE_GROUP_CREATE=true
 CREATE_AZ_DEPLOY_ACI_YAML=true  #@PROJECT_DIR deploy_aci.yml
 CMD_AZ_CREATE_CONTAINERGROUP=true  #.. jeuh - Running ..
-
-GITHUB_DIR=$HOME_DIR/Dropbox/Github/Waardepapieren-AZURE-ACI  #git clone https://github.com/ezahr/Waardepapieren-AZURE-ACI.git 
-PROJECT_DIR=$HOME_DIR/Projects/scratch/virtual-insanity       #git clone https://github.com/disciplo/waardepapieren.git
-DOCKER_COMPOSE_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren
-CLERK_FRONTEND_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/clerk-frontend
-#CLERK_FRONTEND_NGINX_DIR=$CLERK_FRONTEND_DIR/nginx
-#CLERK_FRONTEND_CYPRESS_DIR=$CLERK_FRONTEND_DIR/cypress
-WAARDEPAPIEREN_SERVICE_DIR=$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/waardepapieren-service
-WAARDEPAPIEREN_SERVICE_CONFIG_DIR=$WAARDEPAPIEREN_SERVICE_DIR/configuration
-
 
 # //////////////////////////////////////////////////////////////////////////////////////////
 #echo "#######################"
@@ -165,71 +197,64 @@ clear
 enter_cont
 
 echo "#######################"
-echo "## CERT_HOST_IP "
+echo "## DOWNLOAD"  
 echo "#######################" 
-echo "CERT_HOST_IP"=$CERT_HOST_IP       #discipl.westeurope.azurecontainer.io  #FQDN linux
-echo "CERT_HOST_IP_WAARDEPAPIEREN_SERVICE_HOSTNAME"=$CERT_HOST_IP_WAARDEPAPIEREN_SERVICE_HOSTNAME #discipl.westeurope.azurecontainer.io
-echo "EPHEMERAL_RETENTION_TIME"=$EPHEMERAL_RETENTION_TIME #2592001 #30 dagen
-echo "" 
+echo "GITHUB_DIR="$GITHUB_DIR        # $HOME_DIR/Dropbox/Github/Waardepapieren-AZURE-ACI  #git clone https://github.com/ezahr/Waardepapieren-AZURE-ACI.git 
+echo "PROJECT_DIR="$PROJECT_DIR         #$HOME_DIR/Projects/scratch/virtual-insanity       #git clone https://github.com/disciplo/waardepapieren.git
+echo "DOCKER_COMPOSE_DIR="$DOCKER_COMPOSE_DIR        #$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren
+echo "CLERK_FRONTEND_DIR="$CLERK_FRONTEND_DIR        #$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/clerk-frontend
+#CLERK_FRONTEND_NGINX_DIR="$         #$CLERK_FRONTEND_DIR/nginx
+#CLERK_FRONTEND_CYPRESS_DIR="$         #$CLERK_FRONTEND_DIR/cypress
+echo "WAARDEPAPIEREN_SERVICE_DIR="$WAARDEPAPIEREN_SERVICE_DIR        #$HOME_DIR/Projects/scratch/virtual-insanity/waardepapieren/waardepapieren-service
+echo "WAARDEPAPIEREN_SERVICE_CONFIG_DIR="$WAARDEPAPIEREN_SERVICE_CONFIG_DIR        #$WAARDEPAPIEREN_SERVICE_DIR/configuration
+echo ""
 echo "#######################"
-echo "## Github clean up disposables  tabula rasa "
+echo "## CLONE GITHUB" 
 echo "#######################" 
-echo "CMD_DOCKER_CONTAINER_STOP_AND_PRUNE"=$CMD_CONTAINER_STOP_AND_PRUNE #false
-echo "CMD_DOCKER_IMAGE_REMOVE"=$CMD_IMAGE_REMOVE #false
-echo "CMD_DOCKER_CONTAINER_PRUNE"=$CMD_DOCKER_CONTAINER_PRUNE #false
-echo "CMD_GIT_CLONE"=$CMD_GIT_CLONE #false
-echo "" 
+echo "CMD_CONTAINER_STOP="$CMD_CONTAINER_STOP        #false
+echo "CMD_IMAGE_REMOVE="$CMD_IMAGE_REMOVE         #false
+echo "CMD_CONTAINER_PRUNE="$CMD_CONTAINER_PRUNE         #false
+echo ""
+echo "CMD_GIT_CLONE="$CMD_GIT_CLONE        #false  #git clone https://github.com/disciplo/waardepapieren.git
+echo ""
 enter_cont
+clear
 echo "#######################"
-echo "## Dockerfile  configuration "
+echo "## BUILD "
 echo "#######################" 
-echo "SET_DOCKERCOMPOSE_TRAVIS_WITHOUT_VOLUME"=$SET_DOCKERCOMPOSE_TRAVIS_WITHOUT_VOLUME #true       
-echo "SET_DOCKERFILE_CLERK_FRONTEND_WITHOUT_VOLUME"=$SET_DOCKERFILE_CLERK_FRONTEND_WITHOUT_VOLUME #true
-echo "SET_DOCKERFILE_WAARDEPAPIEREN_WITHOUT_VOLUME"=$SET_DOCKERFILE_WAARDEPAPIEREN_WITHOUT_VOLUME #true
-echo "SET_WAARDEPAPIEREN_SERVICE_CONFIG_COMPOSE_TRAVIS_JSON"=$SET_WAARDEPAPIEREN_SERVICE_CONFIG_COMPOSE_TRAVIS_JSON #true 
-echo "SET_CLERK_FRONTEND_NGINX_CONF"=$SET_CLERK_FRONTEND_NGINX_CONF #true
-echo "" 
-echo "#######################"
-echo "## Docke compose volume  configuration "  # depreciated 
-echo "#######################" 
-echo "SET_DOCKERCOMPOSE_TRAVIS_WITH_VOLUME"=$SET_DOCKERCOMPOSE_TRAVIS_WITH_VOLUME #false
-echo "SET_DOCKERFILE_CLERK_FRONTEND_WITH_VOLUME"=$SET_DOCKERFILE_CLERK_FRONTEND_WITH_VOLUME #false
-echo "SET_DOCKERFILE_WAARDEPAPIEREN_WITH_VOLUME"=$SET_DOCKERFILE_WAARDEPAPIEREN_WITH_VOLUME #false
-echo "" 
-echo "#######################"
-echo "## Dockerfile  build and ship "
-echo "#######################" 
-echo "CMD_DOCKER_COMPOSE"=$CMD_DOCKER_COMPOSE #true
-echo "CMD_DOCKER_BUILD"=$CMD_DOCKER_BUILD #true
-echo "CMD_DOCKER_COMPOSE_BUILD"=#" --build"
-echo "" 
-echo "DOCKER_TAG"=$DOCKER_TAG #true
-echo "DOCKER_USER"=$DOCKER_USER  #"boscp08"  #NB repository name must be lowercase
-echo "DOCKER_VERSION_TAG"=$DOCKER_VERSION_TAG #"3.0"
-
+echo "CMD_DOCKER_COMPOSE="$CMD_DOCKER_COMPOSE        #true  #volumes and links depreciated
+echo "CMD_DOCKER_BUILD="$CMD_DOCKER_BUILD         #false  # build by container
+echo "CMD_DOCKER_COMPOSE_BUILD="$CMD_DOCKER_COMPOSE_BUILD        #" --build"
+echo ""
+echo "SET_DOCKERCOMPOSE_TRAVIS_WITHOUT_VOLUME="$SET_DOCKERCOMPOSE_TRAVIS_WITHOUT_VOLUME         #true       
+echo "SET_DOCKERFILE_CLERK_FRONTEND_WITHOUT_VOLUME="$SET_DOCKERFILE_CLERK_FRONTEND_WITHOUT_VOLUME         #true
+echo "SET_DOCKERFILE_WAARDEPAPIEREN_WITHOUT_VOLUME="$SET_DOCKERFILE_WAARDEPAPIEREN_WITHOUT_VOLUME         #true
+echo ""
+echo "SET_DOCKERCOMPOSE_TRAVIS_WITH_VOLUME="$SET_DOCKERCOMPOSE_TRAVIS_WITH_VOLUME        #false
+echo "SET_DOCKERFILE_CLERK_FRONTEND_WITH_VOLUME="$SET_DOCKERFILE_CLERK_FRONTEND_WITH_VOLUME        #false
+echo "SET_DOCKERFILE_WAARDEPAPIEREN_WITH_VOLUME="$SET_DOCKERFILE_WAARDEPAPIEREN_WITH_VOLUME        #false
+echo ""
+echo "SET_WAARDEPAPIEREN_SERVICE_CONFIG_COMPOSE_TRAVIS_JSON="$SET_WAARDEPAPIEREN_SERVICE_CONFIG_COMPOSE_TRAVIS_JSON        #true 
+echo "SET_CLERK_FRONTEND_NGINX_CONF="$SET_CLERK_FRONTEND_NGINX_CONF        #rue
 enter_cont
-
+clear
 echo "#######################"
-echo "## Ezah ACI Azure Container instance setters"
+echo "## SHIP DOCKER HUB"
 echo "#######################" 
-echo "AZ_RESOURCE_GROUP"=$AZ_RESOURCE_GROUP #"Discipl_Wigo4it_DockerGroup2"
-echo "AZ_DNSNAMELABEL"=$AZ_DNSNAMELABEL #$CERT_HOST_IP | cut -d'.' -f 1  #waardepapieren or disciple
-echo "AZ_RESOURCE_GROUP_DELETE"=$AZ_RESOURCE_GROUP_DELETE #true
-echo "AZ_RESOURCE_GROUP_CREATE"=$AZ_RESOURCE_GROUP_CREATE #true
-echo "CREATE_AZ_DEPLOY_ACI_YAML"=$CREATE_AZ_DEPLOY_ACI_YAML #true  #@PROJECT_DIR deploy_aci.yml
-echo "CMD_AZ_CREATE_CONTAINERGROUP"=$CMD_AZ_CREATE_CONTAINERGROUP #true  #.. jeuh - Running .. portal.azure.com
-echo "" 
-echo "#######################" 
-echo "# Directories used " 
+echo "DOCKER_TAG="$DOCKER_TAG         #true
+echo "DOCKER_USER="$DOCKER_USER       #"boscp08"  #NB repository name must be lowercase
+echo "DOCKER_VERSION_TAG="$DOCKER_VERSION_TAG        #"2.0"
+echo "DOCKER_PUSH="$DOCKER_PUSH         #true  #hub.docker.com 
+echo ""
 echo "#######################"
-echo "HOME_DIR"=$HOME_DIR
-echo "GITHUB_DIR="$GITHUB_DIR
-echo "PROJECT_DIR="$PROJECT_DIR
-echo "DOCKER_COMPOSE_DIR="$DOCKER_COMPOSE_DIR
-echo "CLERK_FRONTEND_CYPRESS_DIR="$CLERK_FRONTEND_CYPRESS_DIR
-echo "WAARDEPAPIEREN_SERVICE_DIR="$WAARDEPAPIEREN_SERVICE_DIR
-echo "WAARDEPAPIEREN_SERVICE_CONFIG_DIR="$WAARDEPAPIEREN_SERVICE_CONFIG_DIR
-
+echo "## DEPLOY AZURE"
+echo "#######################" 
+echo "AZ_RESOURCE_GROUP="$AZ_RESOURCE_GROUP        #"Discipl_Wigo4it_DockerGroup2"
+echo "AZ_RESOURCE_GROUP_DELETE="$AZ_RESOURCE_GROUP_DELETE         #true
+echo "AZ_RESOURCE_GROUP_CREATE="$AZ_RESOURCE_GROUP_CREATE        #true
+echo "CREATE_AZ_DEPLOY_ACI_YAML="$CREATE_AZ_DEPLOY_ACI_YAML        #true  #@PROJECT_DIR deploy_aci.yml
+echo "CMD_AZ_CREATE_CONTAINERGROUP="$CMD_AZ_CREATE_CONTAINERGROUP        #true  #.. jeuh - Running ..
+enter_cont
 # //////////////////////////////////////////////////////////////////////////////////////////
 #echo "#######################"
 #echo "## end of feedbak 
@@ -238,7 +263,7 @@ echo ""
 echo "" 
 echo "hope the run will be okay. "
 enter_cont
-
+clear
 fi
 
 
